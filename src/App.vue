9d5b6b7edd7c37543cbd246508076036e5a95800
nothingct <nothingct@naver.com>
2022-11-06 07:44:00 +0900

    vue3-refs

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <h1 ref="here">
+    Here Comes The Life.
+  </h1> -->
+  <Here ref="here" />
+</template>
+
+ <script>
+import Here from '~/components/Here'
+export default {
+  components:{
+    Here
+  },
+  // created(){
+  //   console.log(this.$refs.here)
+  // },
+  mounted(){
+    // const h1El = document.querySelector('h1')
+    // const h1El = document.querySelector('#here')
+    // console.log(h1El.textContent)
+    console.log(this.$refs.here.$refs.alive)
+  }
+}
+</script>
+
---
+++ src/components/Here.vue
@@ -0,0 +1,7 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Here , Comes the Life.</h1>
+  <h1 ref="alive">
+    Be Alive, Take it
+  </h1>
+</template>
